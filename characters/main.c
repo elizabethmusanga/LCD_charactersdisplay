8e8c8e24153fe0b61127ab0bab56d047d3357e53
elizabeth muanga <elizabeth.n.musanga@gmail.com>
2021-11-09 16:34:45 +0300

    initial commit

---
+++ characters/main.c
@@ -0,0 +1,18 @@
+/*
+ * characters.c
+ *
+ * Created: 11/9/2021 2:05:31 PM
+ * Author : eliza
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
